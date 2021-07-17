63566301df8c6a5da17ce739c5c4d79225ac022e
Johnathan Kelley <jkellz592@gmail.com>
2021-07-17 19:39:22 -0400

    Added a button which reloads & reruns the funtion

---
+++ App.js
@@ -0,0 +1,16 @@
+function generatePassword() {
+    var length = 10,
+    charset = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()",
+    retVal = "";
+    for (var i = 0 , n = charset.length; i < length; i++) {
+        retVal += charset.charAt(Math.floor(Math.random() * n))
+    }
+    return retVal;
+}
+
+document.getElementById("here").innerHTML = generatePassword(); 
+
+
+function refreshPage(){
+    window.location.reload();
+} 
